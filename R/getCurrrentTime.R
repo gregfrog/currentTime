4dc7e9a3b24a46610dff84946a122d2a7a17c96e
Greg <greg@firmansyah.com>
2025-08-09 15:55:37 +1000

    add copyright notices

--- R/getCurrrentTime.R
+++ R/getCurrrentTime.R
@@ -1,4 +1,22 @@
-
+#
+# getCurrentTime - get current time from pool.ntp.org NTP servers 
+#
+#     Copyright (C) 2025  Greg Hunt <greg@firmansyah.com>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
 getCurrentTime<-function()
 {
   hexToRaw <- function(x) {
